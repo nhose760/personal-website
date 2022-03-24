599af619c159d39441b423ea1c2dfc83b7853ded
Nathan <nhose760@cs.washington.edu>
2022-03-23 20:12:42 -0700

    changed app to include routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './index.css';
+
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={ <Projects /> } />
+      <Route pate='/about' element={ <About /> } />
+    </Routes>
   );
 }
 
